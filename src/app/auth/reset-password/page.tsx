f05a864c7a901d33625dd979a2d89ec57c2d2f00
Ravi007ndraGedhar <Ravindra@iwebwiser.com>
2025-07-30 23:47:09 +0530

    add changes in reset-password

--- src/app/auth/reset-password/page.tsx
+++ src/app/auth/reset-password/page.tsx
@@ -3,7 +3,12 @@ import React from 'react'
 export default function page() {
     return (
         <div>
-            reset password page
+            heading: reset password
+            1. input :- 6 box
+            2. reset password
+            3. resend otp
+            4. timer
+
         </div>
     )
 }
